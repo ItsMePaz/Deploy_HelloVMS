b5af5f2e39a98371c0fdcc7238a115bdc8b3fec2
Michael Andrew T. Paz <124416261+ItsMePaz@users.noreply.github.com>
2023-07-13 23:29:00 +0800

    Update visitorService.js

--- frontend/src/services/visitorService.js
+++ frontend/src/services/visitorService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5656/api",
+  baseURL: "https://deploy-hello-api.vercel.app/api",
   headers: {
     common: {
       "Content-Type": "multipart/form-data",
